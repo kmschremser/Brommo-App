0406f97f90d860123886f0efc27141e6b88b43f4
Klaus-M. Schremser <klaus@schremser.com>
2016-11-27 11:44:37 +0100

    removed title

--- application/views/realestate/index.php
+++ application/views/realestate/index.php
@@ -3,23 +3,9 @@
         <div class="right_col" role="main">
           <div class="">
             <div class="page-title">
-              <!--
-              <div class="title_left">
-                <h3>Brommo APP for real estate</h3>
-              </div>
-              -->
 
               <div class="title_right">
-<!--//
-                <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
-                  <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search for...">
-                    <span class="input-group-btn">
-                      <button class="btn btn-default" type="button">Go!</button>
-                    </span>
-                  </div>
-                </div>
-//-->                
+
               </div>
             </div>
             <div class="clearfix"></div>
@@ -74,7 +60,8 @@
 //-->                            
                             <td width="30%" class=" "><a href="<?php echo site_url('realestate/'.$object_item['objid']); ?>"><?php echo $object_item['title']; ?></a></td>
                             <td class=" "><?php echo $object_item['zip']; ?> <?php echo $object_item['city']; ?><br /><?php echo $object_item['street']; ?></td>
-                            <td class=" ">€ <?php echo number_format($object_item['purchaseprice'],2,'.',','); ?> <!--<i class="success fa fa-long-arrow-up"></i>--> <a href="<?php echo site_url('realestate/delete/'.$object_item['objid']); ?>">[X]</a></td>
+                            <td class=" ">€ <?php echo number_format($object_item['purchaseprice'],2,'.',','); ?> <!--<i class="success fa fa-long-arrow-up"></i>--> <a href="<?php echo site_url('realestate/delete/'.$object_item['objid']); ?>">[X]</a><br />
+                              Equity <?php echo number_format($object_item['equityratio']*100,0,'.',','); ?>%</td>
                             <td class=" "><?php echo $object_item['size']; ?> m2 / <?php echo $object_item['outdoorspace']; ?> m2</td>
                             <td width="20%" class="a-right a-right ">Yield <?php echo number_format($object_item['yield'], 2); ?>%<br />
                               Earnings <?php echo number_format($object_item['earnings'], 2); ?> €<br />
@@ -82,7 +69,9 @@
                             </td>
                             <td class=" last">
                               <a href="<?php echo site_url('realestate/'.$object_item['objid']); ?>">[View]</a> | 
-                              <a href="<?php echo site_url('realestate/edit/'.$object_item['objid']); ?>">[Edit]</a>
+                              <a href="<?php echo site_url('realestate/edit/'.$object_item['objid']); ?>">[Edit]</a><br />
+                              <a href="#">^ Upvote</a><br />
+                              <a href="#">v Downvote</a>
                               
                             </td>
                           </tr>
